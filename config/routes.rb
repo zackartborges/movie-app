228eeff902193f79ebb87db985d8f182db537eea
Zack Borges <zackartborges@gmail.com>
2021-02-25 17:57:04 -0500

    adds data to Movies database

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/actors_all" => "actors#individual_actor"
+    get ""
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-actor = Actor.create({ first_name: "Timon", last_name: "Limon", known_for: "Lion King" })
-actor = Actor.create({ first_name: "Dr.", last_name: "Doofenshmirtz", known_for: "Phineas&Ferb" })
-actor = Actor.create({ first_name: "Jim", last_name: "Carrey", known_for: "Bruce Almighty" })
+# actor = Actor.create({ first_name: "Timon", last_name: "Limon", known_for: "Lion King" })
+# actor = Actor.create({ first_name: "Dr.", last_name: "Doofenshmirtz", known_for: "Phineas&Ferb" })
+# actor = Actor.create({ first_name: "Jim", last_name: "Carrey", known_for: "Bruce Almighty" })
+movie = Movie.create ({ title: "One Flew Over the Cuckoo's Nest", year: 1979, plot: "A man decides to go to a mental hospital instead of prison, though he is not mentally challenged." })
+movie = Movie.create ({ title: "Hereditary", year: 2018, plot: "This family just keeps getting crazier!" })
