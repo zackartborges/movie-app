e240c4a6057819970d2c01d02c20f7ed022e9a7a
Zack Borges <zackartborges@gmail.com>
2021-02-25 19:58:17 -0500

    adds json route and paths for individual movie display

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,3 +1,11 @@
 class Api::MoviesController < ApplicationController
-  
+  def all_movie
+    @movie = Movie.all
+    render "movie.json.jb"
+  end
+
+  def individual_movie
+    @movie = Movie.first
+    render "movie.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/actors_all" => "actors#individual_actor"
-    get ""
+    get "/individual_movie_path" => "movies#individual_movie"
+    get "/all_movies_path" => "movies#all_movie"
   end
 end
