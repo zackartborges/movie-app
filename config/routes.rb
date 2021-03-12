0c84c8223515f52b90e3e91d5bf78713f7cdd672
Zack Borges <zackartborges@gmail.com>
2021-03-11 20:32:06 -0500

    adds user controller and model, validations

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
     post "/movies" => "movies#create"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
+
+    post "/users" => "users#create"
   end
 end
