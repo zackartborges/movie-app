edbb94dfdf71c8ec9957443e37939f2e9bacb34f
Zack Borges <zackartborges@gmail.com>
2021-03-24 18:34:34 -0400

    adds genre routes and views

---
+++ app/controllers/api/movie_genres_controller.rb
@@ -0,0 +1,13 @@
+class Api::MovieGenresController < ApplicationController
+  def create
+    @movie_genre = MovieGenre.new(
+      movie_id: params[:movie_id],
+      genre_id: params[:genre_id],
+    )
+    if @movie_genre.save
+      render "show.json.jb"
+    else
+      render json: { error: "sorry" }
+    end
+  end
+end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+# Avoid  issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing () in order to accept cross-origin AJAX requests.
 
-# Read more: https://github.com/cyu/rack-cors
+# Read more: https://github.com/cyu/rack-
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack:: do
 #   allow do
 #     origins 'localhost:8080', 'example.com'
 #
